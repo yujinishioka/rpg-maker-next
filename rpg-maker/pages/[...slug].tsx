d2dcd04f4e563fb304a7b95e43ffcc931fa4e0d6
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2024-04-04 18:47:58 -0300

    fix: ajuste no tamanho de tela

--- rpg-maker/pages/[...slug].tsx
+++ rpg-maker/pages/[...slug].tsx
@@ -5,10 +5,10 @@ export default function Slug():any {
   const { slug } = router.query;
 
   if (router.isFallback || !slug) {
-    return <div className='container min-h-[calc(100vh-10rem)] my-6 items-center'>Carregando...</div>;
+    return <div className='container min-h-[calc(100vh-10.4rem)] my-6 items-center lg:min-h-[calc(100vh-10rem)]'>Carregando...</div>;
   }
 
-  return <div className="container min-h-[calc(100vh-10rem)] my-6">
+  return <div className="container min-h-[calc(100vh-10.4rem)] my-6 lg:min-h-[calc(100vh-10rem)]">
     <h1 className='uppercase'>{slug}</h1>
     <p>slug: {slug}</p>
   </div>
